755ed5e8224741354eca32bd016734f3ce6aaadb
Yi Wang <yiwang1982@gmail.com>
2015-12-22 17:35:28 -0500

    added template code

--- Coffee/CoffeeAPI.swift
+++ Coffee/CoffeeAPI.swift
@@ -33,31 +33,86 @@ class CoffeeAPI {
     }
     
     
-    func getCoffeeShopsWithLocation(location: CLLocation) {
-        
-        if let session = self.session {
-            
-            
+    
+    /*
+    
+    func getCoffeeShopsWithLocation(location:CLLocation)
+    {
+        if let session = self.session
+        {
             var parameters = location.parameters()
-            parameters += [Parameter.categoryId: "4bf58dd8d48988d1e0931735"]  // string is just the hard-coded ID for the “Coffeeshops” category on Foursquare
+            parameters += [Parameter.categoryId: "4bf58dd8d48988d1e0931735"]
             parameters += [Parameter.radius: "2000"]
             parameters += [Parameter.limit: "50"]
-            /*
+            
             // Start a "search", i.e. an async call to Foursquare that should return venue data
-            let searchTask =  session.venues.search(parameters) {
-                
-                
-                
-            }*/
+            let searchTask = session.venues.search(parameters)
+                {
+                    (result) -> Void in
+                    
+                    if let response = result.response
+                    {
+                        if let venues = response["venues"] as? [[String: AnyObject]]
+                        {
+                            autoreleasepool
+                                {
+                                    let realm = try! Realm()
+                                    realm.beginWrite()
+                                    
+                                    for venue:[String: AnyObject] in venues
+                                    {
+                                        let venueObject:Venue = Venue()
+                                        
+                                        if let id = venue["id"] as? String
+                                        {
+                                            venueObject.id = id
+                                        }
+                                        
+                                        if let name = venue["name"] as? String
+                                        {
+                                            venueObject.name = name
+                                        }
+                                        
+                                        if  let location = venue["location"] as? [String: AnyObject]
+                                        {
+                                            if let longitude = location["lng"] as? Float
+                                            {
+                                                venueObject.longitude = longitude
+                                            }
+                                            
+                                            if let latitude = location["lat"] as? Float
+                                            {
+                                                venueObject.latitude = latitude
+                                            }
+                                            
+                                            if let formattedAddress = location["formattedAddress"] as? [String]
+                                            {
+                                                venueObject.address = formattedAddress.joinWithSeparator(" ")
+                                            }
+                                        }
+                                        
+                                        realm.add(venueObject, update: true)
+                                    }
+                                    
+                                    do {
+                                        try realm.commitWrite()
+                                        print("Committing write...")
+                                    }
+                                    catch (let e)
+                                    {
+                                        print("Y U NO REALM ? \(e)")
+                                    }
+                            }
+                            
+                            NSNotificationCenter.defaultCenter().postNotificationName(API.notifications.venuesUpdated, object: nil, userInfo: nil)
+                        }
+                    }
+            }
+            
+            searchTask.start()
         }
-        
     }
-    
-    
-    
-    
-    
-    
+    */
     
     
     
