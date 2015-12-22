e9bc796967b967eb5b2f23c419e1cd993bef99bd
Yi Wang <yiwang1982@gmail.com>
2015-12-22 14:37:53 -0500

    got info.plist permission for requesting location added API file

---
+++ Coffee/CoffeeAPI.swift
@@ -0,0 +1,20 @@
+//
+//  CoffeeAPI.swift
+//  Coffee
+//
+//  Created by Terry Wang on 12/22/15.
+//  Copyright © 2015 Vento. All rights reserved.
+//
+
+import Foundation
+import QuadratTouch
+import RealmSwift
+import MapKit
+
+
+
+struct API {
+    struct notifications {
+        static let venuesUpdated = "venues updated"
+    }
+}
--- Coffee/ViewController.swift
+++ Coffee/ViewController.swift
@@ -7,19 +7,69 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    
+    var locationManager: CLLocationManager?
+    let distanceSpan:Double = 500
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let mapView = self.mapView {
+            mapView.delegate = self
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if locationManager == nil {
+            locationManager = CLLocationManager()
+            
+            locationManager!.delegate = self
+            locationManager!.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+            locationManager!.requestAlwaysAuthorization()
+            locationManager!.distanceFilter = 50 // dont send location update with a distance smaller than 50 meters between them. 
+            
+            locationManager!.startUpdatingLocation()
+            
+            
+        }
+        
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // MARK: - Location Manager Delegate
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        
+        if let mapView = self.mapView {
+            let region = MKCoordinateRegionMakeWithDistance(newLocation.coordinate, distanceSpan, distanceSpan)
+            mapView.setRegion(region, animated: true)
+        }
+        
+    }
+    
+    
 
 }
 
